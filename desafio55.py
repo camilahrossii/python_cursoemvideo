8af3dc0f4e0dc6c1950331b5d2bd9255b6fb9350
camilahrossii <camilarossi.contact@gmail.com>
2023-04-26 20:35:44 -0300

    add files

--- desafio55.py
+++ desafio55.py
@@ -3,24 +3,26 @@
 # qual é o nome do homem mais velho e
 # quantas mulheres têm menos de 20 anos.
 
-velho = 0
-nova = 0
-soma = 0
-for c in range(1,5):
-    print('{} {}ª PESSOA --{}'.format(('-'*12), c, ('-'*12)))
-    nome = input('Nome: ')
+médiaidade = 0
+somaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+for p in range(1,5):
+    print('------ {}ª PESSOA ------'.format(p))
+    nome = str(input('Nome: '))
     idade = int(input('Idade:'))
-    sexo = input('Sexo [F / M]: ')
-    soma += idade
-    if idade == 1:
-        nova = 0
-        velho = 0
-    else:
-        if idade > velho:
-            velho = idade
-        else :
-            nova = idade
-    if sexo == M and idade > nova:
-        print(nome, idade)
-media = soma / 5
-print('A média das idade citada é {}'.format(media))
+    sexo = str(input('Sexo [F / M]: ').strip())
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+            totmulher20 += 1
+médiaidade = somaidade / 4
+print('A média de idade do grupo é de {} anos.'.format(médiaidade))
+print('O homem mais velho é o {}, {}.'.format(nomevelho, maioridadehomem))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(totmulher20))
